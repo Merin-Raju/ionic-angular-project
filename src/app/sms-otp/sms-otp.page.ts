8c2f206346d0e4450f35caa4386ed5b3c4f190a4
Merin-Raju <merin.r@perfomatix.com>
2021-08-18 16:27:08 +0530

    create smt otp

---
+++ src/app/sms-otp/sms-otp.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sms-otp',
+  templateUrl: './sms-otp.page.html',
+  styleUrls: ['./sms-otp.page.scss'],
+})
+export class SmsOtpPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
